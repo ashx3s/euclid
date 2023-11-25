643917f96272c83e67c90180423f6072b3577773
Ashlyn Knox <ashlyn@lilyx.io>
2023-11-25 13:18:59 -0700

    pointList and label setup
    Signed-off-by: Ashlyn Knox <ashlyn@lilyx.io>

--- src/helpers/setLabel.ts
+++ src/helpers/setLabel.ts
@@ -1,8 +1,17 @@
 // generate list of possible labels
-let labelRange = Array.from(Array(26)).map((_, num) =>
+let allLabels = Array.from(Array(26)).map((_, num) =>
   String.fromCharCode(num + 97)
 );
 
-function setLabel(text: string): { label: string } {
-  return { label: text };
+// update pointList with point objects (label, x, y)
+
+let pointList = [];
+
+// when called it should return a label that can be added to a point
+
+function setLabel(x: number, y: number): { label: string } {
+  pointList.push(allLabels.slice(0, 1).toString());
+  const outputLabel = allLabels.slice(0, 1).toString();
+
+  return { label: outputLabel };
 }
