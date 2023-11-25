72e874d6cbdb6610c55f03cd8874d09bcc31bb18
Ashlyn Knox <ashlyn@lilyx.io>
2023-11-25 12:45:56 -0700

    function to create a range of labels
    Signed-off-by: Ashlyn Knox <ashlyn@lilyx.io>

--- src/helpers/setLabel.ts
+++ src/helpers/setLabel.ts
@@ -1,3 +1,8 @@
+// generate list of possible labels
+let labelRange = Array.from(Array(26)).map((_, num) =>
+  String.fromCharCode(num + 97)
+);
+
 function setLabel(text: string): { label: string } {
   return { label: text };
 }
