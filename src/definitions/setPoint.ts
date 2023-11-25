1c450850afce0c634042441d232e7a6209207cff
Ashlyn Knox <ashlyn@lilyx.io>
2023-11-25 12:19:44 -0700

    setPoint init
    Signed-off-by: Ashlyn Knox <ashlyn@lilyx.io>

--- src/definitions/setPoint.ts
+++ src/definitions/setPoint.ts
@@ -1,3 +1,20 @@
-export const setPoint(x:number,y:number) => {
-
+interface point {
+  x: number;
+  y: number;
+  label: string;
 }
+
+/*
+TYPES OF INPUTS
+1. a set of coordinates to create a new point
+*/
+
+// return a point that has x,y coords and a label value
+function setPoint(x: number, y: number, label: string): point {
+  // check if a label has been passed and assign one if not
+
+  // verify that the coordinate points are in the viewbox
+  return { x: x, y: y, label: label };
+}
+
+export { setPoint };
